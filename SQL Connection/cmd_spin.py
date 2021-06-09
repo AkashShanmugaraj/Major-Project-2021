48ba74aa357fc0274264188f0639c9e9f4ca857c
AkashShanmugaraj <akashthelezend@gmail.com>
2021-06-09 09:02:05 +0530

    Duplicated the cmd_spin.py
    This is required to run the Login.py

---
+++ SQL Connection/cmd_spin.py
@@ -0,0 +1,17 @@
+# This module is used to create a spin effect or loading animation which is certainly cool
+# The statement can be customised by providing any string parameter inside the spin function
+# This can be used almost anywhere in the program and the only place where it is a problem is the time when the program
+# connects to a database.... Google is always there
+
+def spin(statement):
+    import time
+    from datetime import datetime
+    symb = ["/","-","\\", "|"]
+    symb.extend(symb+symb)
+    symb.append("done")
+    for i in symb:
+        print(statement,i,end = "\r")
+        time.sleep(0.25)
+    print()
+spin("Connecting to database.....")
+spin("Retrieving Credentials.....")
