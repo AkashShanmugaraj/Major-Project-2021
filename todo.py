a111f5ae1410d44261560e0cce685c4125b544f0
AkashShanmugaraj <69229736+AkashShanmugaraj@users.noreply.github.com>
2021-06-15 09:49:40 +0530

    Added Todo on 15-6-21

--- todo.py
+++ todo.py
@@ -3,5 +3,12 @@ TODO:
     1. Add Transaction ID
     2. Add provision for returning books
     3. Receipt PDF generation
-    4. Implement carriage return
+    4. Find way to clear screen.
+    
+Solutions Found for:
+4. Use the below code
+
+from os import system
+system('cls')
+Ref: https://www.geeksforgeeks.org/clear-screen-python/#:~:text=From%20os%20import%20system.,last%20output%20in%20an%20underscore).
 '''
