456f2ce00016d75c66c9e4356b9edd56a03d38c7
AkashShanmugaraj <akashthelezend@gmail.com>
2021-06-21 15:32:07 +0530

    Added and Modifed some SQL modules

--- SQL Connection/sqlcommands.py
+++ SQL Connection/sqlcommands.py
@@ -6,20 +6,21 @@
 import mysql.connector as mysql
 #Userdefined....found in same folder
 from tabulate import tabulate #For tabulating data
-from loops import intsyntaxcheck, bookquantloop, stringvaluecontrol
+from loops import *
 from cmd_spin import spin
 #Python Modules
 from datetime import datetime
 from os import system
 from time import sleep
+import random
 import keyboard as kb
 import pyfiglet
+
 #Userdefined
 db = mysql.connect(host='localhost', database='bookstore', user='root', password='nevermindmf')
 cur = db.cursor()
 
 #Non SQl Funcitons
-# When we fetch records from a database, it will be a tuple in a list. The below function changes it to list in a list (nested list). This format is crucial to tabulate the data
 def tupletolist(thetuple):
     newlist = []
     for i in thetuple:
@@ -27,14 +28,27 @@ def tupletolist(thetuple):
             newlist.append(ii)
     return newlist
 
-# After a select command is executed, this function can be called. This will fetch the records 
+def transidgen():
+    cur.execute('select TransactionID from transactions')
+    data = cur.fetchall()
+    def tid_gen():
+        numid = ''
+        for i in range(6):
+            numid += str(random.randint(0, 9))
+        return numid
+    num = tid_gen()
+    existingids = tupletolist(data)
+
+    while num in existingids:
+        num = tid_gen()
+    return num
+
 def tabulating():
     rawdata = cur.fetchall()
     newdata = []
     for data in rawdata:
         newdata.append(list(data))
     return newdata
-
 # Function for adding a book
 def add_book():
     databool = True
@@ -47,9 +61,8 @@ def add_book():
     bstock = intsyntaxcheck("Enter available stock: ")
     #bprice = (input("Enter Price (without currency notation): "))
     bprice = intsyntaxcheck('Enter Price (without currency notation): ')
-    bstatus = input("Enter Status of book (InStock, OutofStock, Preorder): ")
     print(bpubyear,"\n\n")
-    cur.execute(f"insert into books values('{bid}','{bname}','{bgenre}','{bauthor}',{bpubyear}, {bstock},{bprice},'{bstatus}');")
+    cur.execute(f"insert into books values('{bid}','{bname}','{bgenre}','{bauthor}',{bpubyear}, {bstock},{bprice});")
     db.commit()
     cur.execute(f"select * from books where BookID =  '{bid}'")
     rawdata = cur.fetchall()
@@ -57,7 +70,7 @@ def add_book():
     for data in rawdata:
         newdata.append(list(data))
     print("The Following record was added: ")
-    print(tabulate(newdata, headers=["BookID", "Book Name", "Genre", "Author", 'Published Year', 'Stocks Available','Price', 'Status of Book'],
+    print(tabulate(newdata, headers=["BookID", "Book Name", "Genre", "Author", 'Published Year', 'Stocks Available','Price'],
                    tablefmt='fancy_grid'))
 
 
@@ -132,7 +145,8 @@ def newtransaction():
         custphnum = int(input("Reconfirm the Phone number of the customer: "))
         spin('Processing Transaction.....')
         today = datetime.now()
-        cur.execute(f"INSERT INTO transactions VALUES ('{today}',{custphnum},'{bid}',{bnum});")
+        id = int(transidgen())
+        cur.execute(f"INSERT INTO transactions VALUES ('{today}',{id}, '{bid}',{bnum},{custphnum});")
         cur.execute(f"UPDATE books SET Stocks={bstock-bnum} WHERE BookID='{bid}';")
         db.commit()
         print("\nTransaction was Sucessful.\nYou will be redirected to home screen now")
@@ -145,9 +159,41 @@ def newtransaction():
         sleep(5)
         system('cls')
         home()
+def viewtransaction():
+    cur.execute('select * from transactions')
+    transdata = tabulating()
+    print("Existing Records are: ")
+    print(tabulate(transdata, headers=["Date", "Customer Phone Number", 'BookID', 'Purchased Quantity'], tablefmt='fancy_grid'))
+
+def viewbooks():
+    menu2 = '''How do you want to have the data displayed?
+        1. Just as it is
+        2. Ordering Books Name in Ascending Order
+        3. Ordering Books Name in Descending Order
+        4. Ordering Author Name in Ascending Order
+        5. Ordering Author Name in Descending Order
+    .....'''
+    selection = integervaluecontrol([1,2,3], menu2, 'Choose only anyone of the following (1,2,3)!!')
+    if selection == 1:
+        cur.execute('select * from books')
+    elif selection == 2:
+        cur.execute('select * from books order by Bookname')
+    elif selection == 3:
+        cur.execute('select * from books order by Bookname desc')
+    elif selection == 4:
+        cur.execute('select * from books order by Author')
+    elif selection == 5:
+        cur.execute('select * from books order by Author desc')
+    data = tabulating()
+    print(tabulate(data, headers=['Book ID','Book Name', 'Book Genre', 'Author', 'Year of Publication', 'Available Stocks', 'Price'], tablefmt='fancy_grid'))
+    redirect = stringvaluecontrol(['y'], "Press y when you are ready to be redirected to the home screen: ","Only permitted value is 'y'!!")
+    system('cls')
+    redirect_home()
+
 def home():
     print(pyfiglet.figlet_format("Welcome Akash Shanmugaraj", font = "digital" ))
-    q = int(input("Do you want to \n\t1. add a book?\n\t2. edit a book? \n\t3. create a new transaction\n\t4.view transactions?\n\t7. Exit App\n...... "))
+    menu = '''Do you want to \n\t1. add a book?\n\t2. edit a book? \n\t3. create a new transaction\n\t4.view transactions?\n\t5.view books?\n\t7. Exit App\n...... '''
+    q = integervaluecontrol([1,2,3,4,5,6,7], menu, '\nChoose one only from the following [1,2,3,4,5,6,7]!!')
     if q == 1:
         system('cls')
         add_book()
@@ -160,14 +206,15 @@ def home():
     elif q == 4:
         system('cls')
         viewtransaction()
+    elif q == 5:
+        system('cls')
+        viewbooks()
     elif q == 7:
         exit()
+
 def redirect_home():
     system('cls')
     home()
-
-
-
 
 print(pyfiglet.figlet_format("Book store Management App", font="slant"))
 home()
