77172077b965363a2391ac87939183da361a901f
AkashShanmugaraj <akashthelezend@gmail.com>
2021-08-19 21:47:28 +0530

    Duplicated encrypt.pt

---
+++ OAuth/encrypt.py
@@ -0,0 +1,31 @@
+from cryptography.fernet import Fernet
+import json
+import cryptocode
+import os,signal
+key = Fernet.generate_key()
+fernet = Fernet(key)
+
+
+def savecred(username, fullname):
+    savefile = open('credentials.dat', 'w')
+
+    tempdict = {"username": username, "name": fullname}
+    tempstring = str(tempdict)
+    encoded = cryptocode.encrypt(tempstring,'lez')
+    savefile.write(encoded)
+
+
+
+def readcred():
+    try:
+        credfile = open('credentials.dat','r')
+        data = credfile.read()
+        decoded = cryptocode.decrypt(data, "lez")
+        decoded = decoded.replace("'", "\"")
+
+        decodeddict = json.loads(decoded)
+        return decodeddict
+    except FileNotFoundError:
+        return False
+a = input('Enter something: ')
+print(a)
