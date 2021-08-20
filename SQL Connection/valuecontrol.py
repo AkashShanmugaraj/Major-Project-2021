6e74a00ca31e37607cb243cface755368b88a6cf
AkashShanmugaraj <akashthelezend@gmail.com>
2021-08-20 15:27:14 +0530

    Update valuecontrol.py

--- SQL Connection/valuecontrol.py
+++ SQL Connection/valuecontrol.py
@@ -1,38 +1,6 @@
 #This custom module has some functions which are used for looping the input statements
-#For example.....
-#Consider that we need to get an input of the number of books required by the user
-#CASE1 - If the user enters a non numerical character, then program will immediately quit.
-#CASE2 - If he enters a number which is more than the books current stock, database will collapse
-#There fore in order to prevent the above cases, we have two functions below
 import os
-#Function for controlling what goes into the bookquantity feild
-#This function will act as a solution for both problem cases.
-def bookquantloop(availablebooks):
-    print(f"Note only {availablebooks} books are available")
-    a = intsyntaxcheck("Enter Required Books: ")
-    while a > availablebooks:
-        print(f"Only {availablebooks} Books are available.")
-        a = intsyntaxcheck("Enter Required Books: ")
-    else:
-        print(f"{a} book(s) were requested")
-        return a
 
-#Functions for Syntax Check
-#This function acts only as a syntax checker for integer datatype
-def intsyntaxcheck(inputstatement):
-
-    try:
-        a = input(inputstatement)
-        if a in ['\\back', '\\home']:
-            print('Exceptions Called')
-        else:
-            a = int(a)
-            return a
-
-    except ValueError:
-        print("Only integers (1,43,532) are allowed!")
-        a = intsyntaxcheck(inputstatement)
-        return a
 
 def stringvaluecontrol(listofpermitttedvalues, inputstatement, errormessage, homefunc = None):
     inputval = input(inputstatement)
