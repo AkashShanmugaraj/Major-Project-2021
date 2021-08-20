9ceacc9194fcebfab77e00cab7ec3d1169c35ce3
AkashShanmugaraj <akashthelezend@gmail.com>
2021-08-20 09:10:04 +0530

    Delete test.py

--- SQL Connection/test.py
+++
@@ -1,6 +0,0 @@
-menu2 = '''How do you want to have the data displayed?
-    1. Just as it is
-    2. Ordering Books Name in Ascending Order
-    3. Ordering Books Name in Descending Order
-.....'''
-a = int(input(menu2))
