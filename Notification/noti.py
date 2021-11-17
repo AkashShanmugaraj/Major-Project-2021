5afb199ecac57c2f980be0369395b9240f932af2
AkashShanmugaraj <akashthelezend@gmail.com>
2021-11-17 19:28:33 +0530

    updated CLI Notification

--- Notification/noti.py
+++ Notification/noti.py
@@ -1,4 +1,5 @@
 import mysql.connector as mysql
+import os
 db = mysql.connect(host='localhost', user = 'root', passwd = 'nevermindmf', database = 'bookstore')
 cur = db.cursor()
 
@@ -6,16 +7,32 @@ cur = db.cursor()
 def notiview():
     cur.execute('select * from notification where seenvar is not null')
     unseendata = cur.fetchall()
-    print("Unseen Data: ")
-    print(unseendata)
+    for i in unseendata:
+        print(i[0] + '   (NEW!)')
     cur.execute('select * from notification where seenvar is null')
     seendata = cur.fetchall()
-    print("Seen Data: ")
-    print(seendata)
+    for i in seendata:
+        print(i[0])
 
     cur.execute('UPDATE notification SET seenvar = null;')
     db.commit()
 
-q = int(input("Enter 1 to view notifications\n"))
-if q == 1:
+
+
+cur.execute('select * from notification where seenvar is not null')
+unseendata = cur.fetchall()
+totalunseen = len(unseendata)
+
+menu = f'''What do you want to do?
+1. Manage Books
+2. Manage Employees
+3. Manage Transactions
+4. View Notifications ({totalunseen} new notifications)
+5. View Profile
+'''
+
+q = int(input(menu))
+
+if q == 4:
+    os.system('cls')
     notiview()
